4ea48c81e9627d3a6ea3442cf33cdb19f653d4d2
nishamgr <nmagar@mail.sfsu.edu>
2026-06-05 19:56:50 -0700

    list-comprehensions review done

--- assignment3/list-comprehensions.py
+++ assignment3/list-comprehensions.py
@@ -1,15 +1,20 @@
 # # Task 3: List Comprehensions Practice
 
 import csv
-
-with open("../csv/employees.csv", newline="") as file:
+try:
+    with open("../csv/employees.csv", newline="") as file:
     # converts to list to slice
-    reader = list(csv.reader(file))
+        reader = list(csv.reader(file))
     # skips header
-    list_of_names = [item[0] + " " + item[1] for item in reader[1:]]
+        list_of_names = [f"{item[1].strip()} {item[2].strip()}" for item in reader[1:]]    
     # to get the names with 'e'
-    list_of_e = [item for item in list_of_names if 'e' in item]
+        list_of_e = [name for name in list_of_names if 'e' in name.lower()]
 
 
-    print(list_of_names)
-    print(list_of_e)
+        print("Full Names (col 1 + col 2):")
+        print(list_of_names)
+    
+        print("\List containing 'e':")
+        print(list_of_e)
+except FileNotFoundError:
+    print("Error couldn't find '/..csv/employee.csv'.")
